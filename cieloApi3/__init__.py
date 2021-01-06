92efe023c858ab86def3d370cd4ce8691e76b826
abnerpalmeira <abnerpalmeira@gmail.com>
2021-01-06 07:42:07 -0300

    feat:  add payment facilitator

--- cieloApi3/__init__.py
+++ cieloApi3/__init__.py
@@ -3,8 +3,11 @@ from .merchant import *
 
 from .sale import *
 from .customer import *
+from .paymentfacilitator import *
+from .subestablishment import *
 from .creditCard import *
 from .payment import *
+from .address import *
 
 from .recurrentPayment import *
 
